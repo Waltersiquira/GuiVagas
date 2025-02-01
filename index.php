e03e7ad3a894349f84281c6fc3406442994eb8f4
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-02-01 15:11:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <title>GuiVagas</title>
+</head>
+<body>
+    <button><a href="index.php" style="text-decoration: none;">Adcionar Vaga</a></button>
+    <?php 
+    require_once 'includes/sistema.php';
+    require_once 'layouts/direitos.php';
+    ?>
+    <h1>GuiVagas</h1>
+    <hr>
+    <table>
+        <?php 
+        $busca = $sistema->query('select * from vagas');
+        if (!$busca){
+            echo 'Erro';
+        } else {
+            if ($busca->num_rows == 0){
+                echo 'Existe nenhuma vaga no momento';
+            } else {
+                while ($reg=$busca->fetch_object()){
+                    echo "<tr><td><a href='pagina-vaga.php?i=$reg->id'><img src='$reg->imagem'></a></td><td>$reg->nome</td><td><img src='imgs/delete_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png' width='25'></td></tr>";
+                }
+            }
+        }
+        ?>
+    </table>
+    <?php direitos() ?>
+    <?php $sistema->close() ?>
+</body>
+</html>
